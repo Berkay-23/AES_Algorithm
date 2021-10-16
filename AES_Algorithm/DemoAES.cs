0f391bf452190ce775b0522df31544f7a5d168b5
Berkay Çavuşoğlu <64416591+Berkay-23@users.noreply.github.com>
2021-10-16 13:28:42 +0300

    Initial commit

---
+++ AES_Algorithm/DemoAES.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AES_Algorithm
+{
+    public class DemoAES
+    {
+        AesCryptoServiceProvider cryptoServiceProvider;
+
+        public DemoAES()
+        {
+            cryptoServiceProvider = new AesCryptoServiceProvider();
+
+            cryptoServiceProvider.BlockSize = 128;
+            cryptoServiceProvider.KeySize = 256;
+            cryptoServiceProvider.GenerateIV();
+            cryptoServiceProvider.GenerateKey();
+            cryptoServiceProvider.Mode = CipherMode.CBC;
+            cryptoServiceProvider.Padding = PaddingMode.PKCS7;
+        }
+
+        public String Encrypt(String clearText)
+        {
+            ICryptoTransform cryptoTransform = cryptoServiceProvider.CreateEncryptor();
+
+            byte[] encryptedBytes = cryptoTransform.TransformFinalBlock(ASCIIEncoding.ASCII.GetBytes(clearText), 0, clearText.Length);
+            string result = Convert.ToBase64String(encryptedBytes);
+
+            return result;
+        }
+
+        public String Decrypt(String cipherText)
+        {
+            ICryptoTransform cryptoTransform = cryptoServiceProvider.CreateDecryptor();
+
+            byte[] encryptedBytes = Convert.FromBase64String(cipherText);
+            byte[] decryptedBytes = cryptoTransform.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+
+            string result = ASCIIEncoding.ASCII.GetString(decryptedBytes);
+            
+            return result;
+        }
+    }
+}
---
+++ AES_Algorithm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AES_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoAES aesAlgorithm = new DemoAES();
+
+            Console.Write($"Write the TEXT: ");
+            string text = Console.ReadLine();
+
+            string dText = aesAlgorithm.Encrypt(text);
+
+            string eText = aesAlgorithm.Decrypt(dText);
+            
+            Console.WriteLine($"Encrypted TEXT = {dText}\nDecrypted TEXT = {eText}");
+
+            Console.Read();
+        }
+    }
+}
